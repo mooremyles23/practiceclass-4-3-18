74dafe75959eaa3bef5d47b50a4b88f971a45862
Moore <mlmoore.2017@mymail.jcsu.edu>
2018-04-03 13:53:17 -0400

    first commit

---
+++ differentarrays.java
@@ -0,0 +1,15 @@
+
+public class differentarrays {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int[] a = {2, 1, 5};
+	int[] b = {7, 1, 3};
+	int[] c = new int[a.length];
+	for (int i = 0; i < a.length; ++i) {
+	    c[i] = a[i] + b[i];
+	    
+	    
+	    System.out.println(c[i]);
+}}}
